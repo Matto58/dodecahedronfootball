a5525e7e783647c972776342d5e27c034becfccd
Matto58 <thematto58@gmail.com>
2026-06-05 17:11:23 +0200

    forgot to bump version oops

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -3,7 +3,7 @@ extends Node
 class_name Settoing # <-- yes this is a phighting reference. yes it is an intentional typo
 
 # GAME INFO CONSTS
-const GAME_VER = "0.1.0"
+const GAME_VER = "0.1.1"
 const GAME_DEMO_NUM = 1
 
 # PLAYER
