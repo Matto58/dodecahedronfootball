4338524f21fc1b7ad2093636983eab4a65772207
Matto58 <thematto58@gmail.com>
2026-06-06 08:59:37 +0200

    set default resolution to 1600x900 to fit on smaller screens, bump to demo 2, bump to 0.2.0

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -3,8 +3,8 @@ extends Node
 class_name Settoing # <-- yes this is a phighting reference. yes it is an intentional typo
 
 # GAME INFO CONSTS
-const GAME_VER = "0.1.2"
-const GAME_DEMO_NUM = 1
+const GAME_VER = "0.2.0"
+const GAME_DEMO_NUM = 2
 
 # PLAYER
 static var rotMod: float = 0.05
