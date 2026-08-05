6f13e8416685572bfd74bdc78b0dc58cfde08f9e
Matto58 <thematto58@gmail.com>
2026-08-05 16:03:13 +0200

    move console logger to a separate file

--- scripts/server/console.gd
+++ scripts/server/console.gd
@@ -53,24 +53,6 @@ var commandsHelp: Dictionary[String, String] = {
 	"map kick_bot": "[nickname] kicks the specified bot",
 }
 
-class ConsoleLogger extends Logger:
-	var uiLog: RichTextLabel
-	func _log_error(function: String, file: String, line: int, code: String, rationale: String, editor_notify: bool, error_type: int, script_backtraces: Array[ScriptBacktrace]) -> void:
-		if uiLog == null: return
-		uiLog.push_paragraph(HORIZONTAL_ALIGNMENT_LEFT)
-		uiLog.push_color(Color.YELLOW if error_type == ERROR_TYPE_WARNING else Color.RED)
-		uiLog.add_text("[%s] %s: %s (%s at %s:%d)" % [Time.get_time_string_from_system(), "WARN" if error_type == ERROR_TYPE_WARNING else "ERROR", rationale, code, file, code])
-		uiLog.pop()
-		uiLog.pop()
-
-	func _log_message(message: String, error: bool) -> void:
-		if uiLog == null: return
-		uiLog.push_paragraph(HORIZONTAL_ALIGNMENT_LEFT)
-		uiLog.push_color(Color.RED if error else Color.LIGHT_SLATE_GRAY)
-		uiLog.add_text("[%s] %s" % ["ERROR" if error else "INFO", message])
-		uiLog.pop()
-		uiLog.pop()
-
 func runCmd(cmd: String):
 	%conlog.push_paragraph(HORIZONTAL_ALIGNMENT_LEFT)
 	%conlog.add_text("> " + cmd)
